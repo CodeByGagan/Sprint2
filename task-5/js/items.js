a251a50bdd1edcba47bcd83aba6ac45d89b21e74
Gagan-0 <gagan0kumar00@gmail.com>
2023-07-31 18:11:12 +0530

    added back home button

--- task-5/js/items.js
+++ task-5/js/items.js
@@ -1,19 +1,25 @@
 // Initialize a new TaskManager with currentId set to 0
 const itemsController = new ItemsController(0);
 
-
-function addItemCard(item){
-    const itemHTML = '<div class="card" style="width: 20rem;">\n' +
-        '    <img src="'+item.img +'" width="300" height="250"  alt="product image">\n' +
-        '    <div class="card-body">\n' +
-        '        <h5 class="card-title">'+item.name+'</h5>\n' +
-        '        <p class="card-text">'+item.description+'</p>\n' +
-        '        <a href="#" class="btn btn-primary">Add</a>\n' +
-        '    </div>\n' +
-        '</div>\n' +
-        '<br/>';
-    const itemsContainer = document.getElementById("list-items");
-    itemsContainer.innerHTML += itemHTML;
+function addItemCard(item) {
+  const itemHTML =
+    '<div class="card" style="width: 20rem;">\n' +
+    '    <img src="' +
+    item.img +
+    '" width="300" height="250"  alt="product image">\n' +
+    '    <div class="card-body">\n' +
+    '        <h5 class="card-title">' +
+    item.name +
+    "</h5>\n" +
+    '        <p class="card-text">' +
+    item.description +
+    "</p>\n" +
+    '        <a href="#" class="btn btn-primary">Add</a>\n' +
+    "    </div>\n" +
+    "</div>\n" +
+    "<br/>";
+  const itemsContainer = document.getElementById("list-items");
+  itemsContainer.innerHTML += itemHTML;
 }
 /*The localStorage object allows you to save key/value pairs in the browser.
 <script>
@@ -29,24 +35,30 @@ structured data based on JavaScript object syntax. It is commonly used for trans
     so it can be displayed on a web page, or vice versa).
 
 */
-function loadStorageSampleData(){
-    if(!localStorage.getItem("items")){
-        const sampleItems = [{'name':'juice',
-        'img':'https://www.gs1india.org/media/Juice_pack.jpg',
-        'description':'Orange and Apple juice fresh and delicious'},
+function loadStorageSampleData() {
+  if (!localStorage.getItem("items")) {
+    const sampleItems = [
+      {
+        name: "juice",
+        img: "https://www.gs1india.org/media/Juice_pack.jpg",
+        description: "Orange and Apple juice fresh and delicious",
+      },
 
-        {'name':'Ruffles Chicken',
-        'img':'https://i5.walmartimages.com/asr/a8187f0a-1bfb-4a45-a54f-c48cb38b8f86.2d572c9d4520b76a1d1a619c2ae3b121.jpeg',
-        'description':'Ruffles Potato Chips - Chicken'}];
-        localStorage.setItem("items", JSON.stringify(sampleItems));
-    }
+      {
+        name: "Ruffles Chicken",
+        img: "https://i5.walmartimages.com/asr/a8187f0a-1bfb-4a45-a54f-c48cb38b8f86.2d572c9d4520b76a1d1a619c2ae3b121.jpeg",
+        description: "Ruffles Potato Chips - Chicken",
+      },
+    ];
+    localStorage.setItem("items", JSON.stringify(sampleItems));
+  }
 }
 
-function loadCardsListFromItemsController(){
-    for(var i = 0, size = itemsController.items.length; i < size ; i++){
-        const item = itemsController.items[i];
-        addItemCard(item);
-    }
+function loadCardsListFromItemsController() {
+  for (var i = 0, size = itemsController.items.length; i < size; i++) {
+    const item = itemsController.items[i];
+    addItemCard(item);
+  }
 }
 
 loadStorageSampleData();
